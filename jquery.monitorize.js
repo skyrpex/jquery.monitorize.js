52f4d0a755cda6dfe580ecc729619af0576a2a05
Cristian Pallarés <skyrpex@gmail.com>
2013-07-23 11:21:35 +0200

    Create jquery.monitorize.js

---
+++ jquery.monitorize.js
@@ -0,0 +1,138 @@
+/**
+* jquery.monitorize.js v1.0
+* Monitorize question changes the smart way
+* https://github.com/skyrpex/jquery.monitorize.js
+*
+* Copyright 2013, Cristian Pallarés
+* Released under the MIT license.
+* https://github.com/skyrpex/jquery.monitorize.js
+*/
+(function ($) {
+    "use strict";
+
+    var methods = {
+        init: function (options) {
+            // Options
+            var o = $.extend({
+                    frequency: 3500,
+                    onQuestionAsked: null,
+                    triggerKeyCodes: [
+                        49,
+                        56,
+                        219,
+                        188,
+                        190,
+                        191,
+                        221,
+                        222
+                    ]
+                }, options),
+                self = this;
+
+            return this.each(function () {
+                // Setup options and private data
+                self.data('monitorize', {
+                    options: o,
+                    _data: {
+                        isQuestionDirty: $(this).val(),
+                        lastQuestion: null,
+                        timer: null
+                    }
+                });
+
+                // Bind keyup event
+                $(this).keyup(function (e) {
+                    var data = $(this).data('monitorize')._data,
+                        options = $(this).data('monitorize').options;
+
+                    // Mark question as dirty
+                    data.isQuestionDirty = true;
+
+                    // Skip if didn't pressed a triggering key code
+                    if (options.triggerKeyCodes.indexOf(e.keyCode) === -1) {
+                        return;
+                    }
+
+                    // Do ask
+                    methods._ask.call(this);
+                });
+
+                // Bind paste event
+                $(this).bind('paste', function () {
+                    // Mark question as dirty
+                    var data = $(this).data('monitorize')._data;
+                    data.isQuestionDirty = true;
+
+                    // Don't ask: value isn't accessible yet
+                    // methods._ask.call(this);
+                });
+
+                // Bind change event
+                $(this).change(function () {
+                    // Mark question as dirty
+                    var data = $(this).data('monitorize')._data;
+                    data.isQuestionDirty = true;
+
+                    // Do ask
+                    methods._ask.call(this);
+                });
+
+                // Start timer
+                methods._restartTimer.call(this);
+            });
+        },
+
+        _ask: function () {
+            // Trim question
+            var question = $.trim($(this).val()),
+                data = $(this).data('monitorize')._data,
+                options = $(this).data('monitorize').options;
+
+            // Skip if question didn't changed
+            if (!data.isQuestionDirty || !question || question === data.lastQuestion) {
+                return;
+            }
+
+            // Ask using the callback
+            options.onQuestionAsked(question);
+
+            // Reset the timer
+            methods._restartTimer.call(this);
+
+            // Update last question
+            data.lastQuestion = question;
+
+            // Question is not dirty anymore
+            data.isQuestionDirty = false;
+        },
+
+        _restartTimer: function () {
+            var self = this,
+                data = $(this).data('monitorize')._data,
+                options = $(this).data('monitorize').options;
+
+            if (data.timer) {
+                clearInterval(data.timer);
+            }
+
+            data.timer = setInterval(function () {
+                methods._ask.call(self);
+            }, options.frequency);
+        }
+    };
+    
+    $.fn.monitorize = function (method) {
+
+        // Method calling logic
+        if (methods[method] && method.charAt(0) !== '_') {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        }
+
+        if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        }
+
+        $.error('Method ' +  method + ' does not exist on jQuery.monitorize');
+
+    };
+})(jQuery);
