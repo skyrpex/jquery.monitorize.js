95eb6aa104416c5ab95ae02d12e6c26f342191e7
Cristian Pallarés <skyrpex@gmail.com>
2014-06-11 14:46:14 +0200

    Update jquery.monitorize.js

--- jquery.monitorize.js
+++ jquery.monitorize.js
@@ -1,5 +1,5 @@
 /**
-* jquery.monitorize.js v1.1
+* jquery.monitorize.js v1.2
 * Monitorize input changes in a smart way
 * https://github.com/skyrpex/jquery.monitorize.js
 *
@@ -7,124 +7,193 @@
 * Released under the MIT license.
 */
 (function (factory) {
-if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module depending on jQuery.
-    define(['jquery'], factory);
-} else {
-    // No AMD. Register plugin with global jQuery object.
-    factory(jQuery);
-}
+
+    if (typeof define === 'function' && define.amd) {
+
+        // AMD. Register as an anonymous module depending on jQuery.
+        define(['jquery'], factory);
+
+    } else {
+
+        // No AMD. Register plugin with global jQuery object.
+        factory(jQuery);
+
+    }
+
 }(function ($) {
+    
     "use strict";
 
     var methods = {
+
         init: function (options) {
+
             // Options
             var o = $.extend({
+
                     frequency: 3500,
-                    onQuestionAsked: null,
+
+                    onValueChanged: null,
+
                     triggerKeyCodes: [
-                        49,
-                        56,
-                        219,
-                        188,
-                        190,
-                        191,
-                        221,
-                        222
-                    ]
+                        // 8,   // Backspace
+                        // 32,  // Space
+                        188, // Comma
+                        190, // Dot
+                        // 49,
+                        // 56,
+                        // 219,
+                        // 188,
+                        // 190,
+                        // 191,
+                        // 221,
+                        // 222
+                    ],
+
+                    skipEmptyValue: true
+
                 }, options),
+
                 self = this;
 
             return this.each(function () {
+
                 // Setup options and private data
                 self.data('monitorize', {
+
                     options: o,
+
                     _data: {
-                        isQuestionDirty: $(this).val(),
-                        lastQuestion: null,
+
+                        isValueDirty: $(this).val(),
+
+                        lastValue: null,
+
                         timer: null
+
                     }
+
                 });
 
                 // Bind keyup event
                 $(this).keyup(function (e) {
+
                     var data = $(this).data('monitorize')._data,
+
                         options = $(this).data('monitorize').options;
 
-                    // Mark question as dirty
-                    data.isQuestionDirty = true;
+                    // Mark value as dirty
+                    data.isValueDirty = true;
 
                     // Skip if didn't pressed a triggering key code
-                    if ($.inArray(e.keyCode, options.triggerKeyCodes) === -1) {
+                    if (options.triggerKeyCodes
+                            && options.triggerKeyCodes.length > 0
+                            && $.inArray(e.keyCode, options.triggerKeyCodes) === -1) {
+
                         return;
+
                     }
 
                     // Do ask
                     methods._ask.call(this);
+
                 });
 
                 // Bind paste event
                 $(this).bind('paste', function () {
-                    // Mark question as dirty
+
+                    // Mark value as dirty
                     var data = $(this).data('monitorize')._data;
-                    data.isQuestionDirty = true;
+
+                    data.isValueDirty = true;
 
                     // Don't ask: value isn't accessible yet
                     // methods._ask.call(this);
+
                 });
 
                 // Bind change event
                 $(this).change(function () {
-                    // Mark question as dirty
+
+                    // Mark value as dirty
                     var data = $(this).data('monitorize')._data;
-                    data.isQuestionDirty = true;
+
+                    data.isValueDirty = true;
 
                     // Do ask
                     methods._ask.call(this);
+
                 });
 
                 // Start timer
                 methods._restartTimer.call(this);
+
             });
+
         },
 
         _ask: function () {
-            // Trim question
-            var question = $.trim($(this).val()),
+
+            // Trim value
+            var value = $.trim($(this).val()),
+
                 data = $(this).data('monitorize')._data,
+
                 options = $(this).data('monitorize').options;
 
-            // Skip if question didn't changed
-            if (!data.isQuestionDirty || !question || question === data.lastQuestion) {
+            // Skip if value didn't changed
+            if (!data.isValueDirty ||value === data.lastValue) {
+
                 return;
+
+            }
+
+            // Skip if empty value
+            if (options.skipEmptyValue && !value) {
+
+                return;
+
             }
 
             // Ask using the callback
-            options.onQuestionAsked(question);
+            options.onValueChanged(value);
 
             // Reset the timer
             methods._restartTimer.call(this);
 
-            // Update last question
-            data.lastQuestion = question;
+            // Update last value
+            data.lastValue = value;
 
-            // Question is not dirty anymore
-            data.isQuestionDirty = false;
+            // Value is not dirty anymore
+            data.isValueDirty = false;
+
         },
 
         _restartTimer: function () {
+
             var self = this,
+
                 data = $(this).data('monitorize')._data,
+
                 options = $(this).data('monitorize').options;
 
+
             if (data.timer) {
+
                 clearInterval(data.timer);
+
             }
 
-            data.timer = setInterval(function () {
-                methods._ask.call(self);
-            }, options.frequency);
+            if (options.frequency) {
+
+                data.timer = setInterval(function () {
+
+                    methods._ask.call(self);
+
+                }, options.frequency);
+
+            }
+
         }
     };
     
@@ -132,14 +201,19 @@ if (typeof define === 'function' && define.amd) {
 
         // Method calling logic
         if (methods[method] && method.charAt(0) !== '_') {
+
             return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+
         }
 
         if (typeof method === 'object' || !method) {
+
             return methods.init.apply(this, arguments);
+
         }
 
         $.error('Method ' +  method + ' does not exist on jQuery.monitorize');
 
     };
+
 }));
