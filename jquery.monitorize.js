ffc7803342ebf6be3e451f42d5863d23ac2f93e2
Skyrpex <skyrpex@gmail.com>
2014-06-12 15:06:53 +0200

    Update jquery.monitorize.js

--- jquery.monitorize.js
+++ jquery.monitorize.js
@@ -1,5 +1,5 @@
 /**
-* jquery.monitorize.js v1.2
+* jquery.monitorize.js v1.3
 * Monitorize input changes in a smart way
 * https://github.com/skyrpex/jquery.monitorize.js
 *
@@ -40,7 +40,9 @@
                         190, // Dot
                     ],
 
-                    skipEmptyValue: true
+                    pasteTriggers: true,
+
+                    emptyValueTriggers: true
 
                 }, options || {}),
 
@@ -66,52 +68,13 @@
                 });
 
                 // Bind keyup event
-                $(this).keyup(function (e) {
-
-                    var data = $(this).data('monitorize')._data;
-
-                    // Mark value as dirty
-                    data.isValueDirty = true;
-
-                    // Skip if didn't pressed a triggering key code
-                    if (options.triggerKeyCodes
-                            && options.triggerKeyCodes.length > 0
-                            && $.inArray(e.keyCode, options.triggerKeyCodes) === -1) {
-
-                        return;
-
-                    }
-
-                    // Maybe call callback
-                    methods._maybeCallCallback.call(this);
-
-                });
+                $(this).on('keyup', methods._onKeyUp);
 
                 // Bind paste event
-                $(this).bind('paste', function () {
-
-                    // Mark value as dirty
-                    var data = $(this).data('monitorize')._data;
-
-                    data.isValueDirty = true;
-
-                    // Don't call callback: value isn't accessible yet
-                    // methods._maybeCallCallback.call(this);
-
-                });
+                $(this).on('paste', methods._onPaste);
 
                 // Bind change event
-                $(this).change(function () {
-
-                    // Mark value as dirty
-                    var data = $(this).data('monitorize')._data;
-
-                    data.isValueDirty = true;
-
-                    // Maybe call callback
-                    methods._maybeCallCallback.call(this);
-
-                });
+                $(this).on('change', methods._onChange);
 
                 // Start timer
                 methods._restartTimer.call(this);
@@ -122,9 +85,77 @@
 
         destroy: function () {
 
+            // Clear timer
             methods._clearTimer.call(this);
 
+            // Unbind keyup event
+            $(this).unbind('keyup', methods._onKeyUp);
+
+            // Unbind paste event
+            $(this).unbind('paste', methods._onPaste);
+
+            // Unbind change event
+            $(this).unbind('change', methods._onChange);
+
+            // Remove monitorize data
             $(this).data('monitorize', null);
+
+        },
+
+        _onKeyUp: function (e) {
+
+            var data = $(this).data('monitorize')._data,
+
+                options = $(this).data('monitorize').options;
+
+            // Mark value as dirty
+            data.isValueDirty = true;
+
+            // Skip if didn't pressed a triggering key code
+            if (options.triggerKeyCodes
+                    && options.triggerKeyCodes.length > 0
+                    && $.inArray(e.keyCode, options.triggerKeyCodes) === -1) {
+
+                return;
+
+            }
+
+            // Maybe call callback
+            methods._maybeCallCallback.call(this);
+
+        },
+
+        _onPaste: function () {
+
+            // Mark value as dirty
+            var data = $(this).data('monitorize')._data,
+
+                options = $(this).data('monitorize').options;
+
+            data.isValueDirty = true;
+
+            // Don't call callback (value isn't accessible yet)
+            if (options.pasteTriggers) {
+
+                setTimeout(function () {
+
+                    methods._maybeCallCallback.call(self);
+
+                }, 0);
+
+            }
+
+        },
+
+        _onChange: function () {
+
+            // Mark value as dirty
+            var data = $(this).data('monitorize')._data;
+
+            data.isValueDirty = true;
+
+            // Maybe call callback
+            methods._maybeCallCallback.call(this);
 
         },
 
@@ -145,7 +176,7 @@
             }
 
             // Skip if empty value
-            if (options.skipEmptyValue && !value) {
+            if (!options.emptyValueTriggers && !value) {
 
                 return;
 
